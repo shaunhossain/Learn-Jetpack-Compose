29a8819991c6a26f6b32eed25cd082a9fa305323
shaun <shaunhossain95@gmail.com>
2022-06-07 21:46:31 +0600

    show hello world in screen

--- app/src/main/java/com/shaunhossain/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/shaunhossain/learnjetpackcompose/MainActivity.kt
@@ -16,28 +16,20 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearnJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Hello world")
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = name)
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
-        Greeting("Android")
+        Greeting("Hello word")
     }
 }
