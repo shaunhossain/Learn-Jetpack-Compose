ff35054bca2df2271feaea4575a21adb6d2973af
shaun <shaunhossain95@gmail.com>
2022-06-10 17:37:10 +0600

    use modifier in column , row , text etc

--- app/src/main/java/com/shaunhossain/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/shaunhossain/learnjetpackcompose/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.shaunhossain.learnjetpackcompose
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,39 +24,40 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //TestColumn(firstString = "hello", secondString = "word")
-            TestRow(firstString = "hello", secondString = "word")
+            TestColumn(firstString = "Click", secondString = "word")
+            //TestRow(firstString = "hello", secondString = "word")
         }
     }
 }
 
 @Composable
-fun TestColumn(firstString: String, secondString: String) {
+fun TestColumn(firstString: String, secondString: String){
     Column(
         modifier = Modifier
+            .background(Color.Blue)
             .fillMaxSize()
-            .background(Color.Green),
+            .border(width = 5.dp, Color.White)
+            .padding(horizontal = 10.dp, vertical = 10.dp)
+            .border(width = 5.dp, Color.White)
+            .border(width = 2.dp, color = Color.Red)
+            .padding(horizontal = 10.dp, vertical = 10.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceBetween
-
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(firstString)
+        Text(firstString, modifier = Modifier
+            .background(Color.Cyan)
+            .padding(10.dp)
+            .clickable {
+
+        })
+        Spacer(modifier = Modifier.height(50.dp))
         Text(secondString)
     }
 }
 
 @Composable
-fun TestRow(firstString: String, secondString: String) {
-    Row(
-        modifier = Modifier
-            //.requiredWidth(200.dp)
-            .height(50.dp)
-            .padding(16.dp)
-            .background(Color.Green)
-            .fillMaxWidth(0.5f),
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.Top
-    ) {
+fun TestRow(firstString: String, secondString: String){
+    Row() {
         Text(firstString)
         Text(secondString)
     }
@@ -64,6 +72,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     LearnJetpackComposeTheme {
-        TestRow("Hello", " word")
+        TestRow("Hello"," word")
     }
 }
